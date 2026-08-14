17b0d4dec86fbe1b282754bb77147bbde2d94826
Tenebrea <andrejbulatov8673@gmail.com>
2026-08-14 19:02:55 +0300

    Create WeekCalendar composable for MainScreen

---
+++ shared/src/commonMain/kotlin/com/example/myhabitapp/presentation/common/SelectableCircle.kt
@@ -0,0 +1,61 @@
+package com.example.myhabitapp.presentation.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.example.myhabitapp.ui.theme.HabitAppTheme
+
+@Composable
+fun SelectableCircle(
+    modifier: Modifier = Modifier,
+    selected: Boolean,
+    text: String
+) {
+    Box(modifier
+        .clip(CircleShape)
+        .background(if (selected) MaterialTheme.colorScheme.surface
+        else MaterialTheme.colorScheme.onSurface),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelSmall,
+            color = if (selected) MaterialTheme.colorScheme.onSurface
+            else MaterialTheme.colorScheme.surface
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun SelectedCirclePreview() {
+    HabitAppTheme {
+        SelectableCircle(
+            modifier = Modifier.size(64.dp),
+            selected = true,
+            text = "12"
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun NotSelectedCirclePreview() {
+    HabitAppTheme {
+        SelectableCircle(
+            modifier = Modifier.size(64.dp),
+            selected = false,
+            text = "12"
+        )
+    }
+}
